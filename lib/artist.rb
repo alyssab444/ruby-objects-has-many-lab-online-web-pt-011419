afa926b4d625704c09191ff29826191122f39eb0
Alyssa Brown <alyssa.brown@live.com>
2019-02-12 03:17:02 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,13 +2,13 @@ class Artist
   attr_accessor :name 
   def initialize(name)
     @name = name 
-    @song = []
+    @songs = []
   end 
   def songs
     @songs 
   end 
   def add_song(song)
-    @song << song 
+    @songs << song 
     song.artist = self 
   end 
 end 
